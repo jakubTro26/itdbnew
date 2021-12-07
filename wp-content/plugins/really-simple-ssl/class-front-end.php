7b5d74e0c4a40679a704266e194998541bff7ce9
kuba <info@weblead.pl>
2021-12-07 14:58:06 +0100

    first

--- wp-content/plugins/really-simple-ssl/class-front-end.php
+++ wp-content/plugins/really-simple-ssl/class-front-end.php
@@ -79,14 +79,7 @@ if ( ! class_exists( 'rsssl_front_end' ) ) {
 
         public function wp_redirect_to_ssl()
         {
-            if (!array_key_exists('HTTP_HOST', $_SERVER)) return;
-
-            if (!is_ssl() && !(defined("rsssl_no_wp_redirect") && rsssl_no_wp_redirect)) {
-                $redirect_url = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-                $redirect_url = apply_filters("rsssl_wp_redirect_url", $redirect_url);
-                wp_redirect($redirect_url, 301);
-                exit;
-            }
+         
         }
 
 
--- wp-includes/pluggable.php
+++ wp-includes/pluggable.php
@@ -1288,7 +1288,61 @@ if ( ! function_exists( 'wp_redirect' ) ) :
 	 * @return bool False if the redirect was cancelled, true otherwise.
 	 */
 	function wp_redirect( $location, $status = 302, $x_redirect_by = 'WordPress' ) {
-		
+		global $is_IIS;
+
+		/**
+		 * Filters the redirect location.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param string $location The path or URL to redirect to.
+		 * @param int    $status   The HTTP response status code to use.
+		 */
+		$location = apply_filters( 'wp_redirect', $location, $status );
+
+		/**
+		 * Filters the redirect HTTP response status code to use.
+		 *
+		 * @since 2.3.0
+		 *
+		 * @param int    $status   The HTTP response status code to use.
+		 * @param string $location The path or URL to redirect to.
+		 */
+		$status = apply_filters( 'wp_redirect_status', $status, $location );
+
+		if ( ! $location ) {
+			return false;
+		}
+
+		if ( $status < 300 || 399 < $status ) {
+			wp_die( __( 'HTTP redirect status code must be a redirection code, 3xx.' ) );
+		}
+
+		$location = wp_sanitize_redirect( $location );
+
+		if ( ! $is_IIS && 'cgi-fcgi' !== PHP_SAPI ) {
+			status_header( $status ); // This causes problems on IIS and some FastCGI setups.
+		}
+
+		/**
+		 * Filters the X-Redirect-By header.
+		 *
+		 * Allows applications to identify themselves when they're doing a redirect.
+		 *
+		 * @since 5.1.0
+		 *
+		 * @param string $x_redirect_by The application doing the redirect.
+		 * @param int    $status        Status code to use.
+		 * @param string $location      The path to redirect to.
+		 */
+		$x_redirect_by = apply_filters( 'x_redirect_by', $x_redirect_by, $status, $location );
+		if ( is_string( $x_redirect_by ) ) {
+			header( "X-Redirect-By: $x_redirect_by" );
+		}
+
+		header( "Location: $location", true, $status );
+
+		return true;
 	}
 endif;
 
